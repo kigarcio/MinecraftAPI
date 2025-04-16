32144ff8b7649833769e3472d5847ce209a05dd0
kigarcio <kigarcio@gmail.com>
2025-04-17 00:35:18 +0200

    Crafting recipes

--- js/main.js
+++ js/main.js
@@ -73,13 +73,12 @@ const getRecipes = () => {
 	fetch(URL_RECIPES, options)
 		.then((res) => res.json())
 		.then((data) => {
+			
 			const searchedRecipes = data.find(
 				(recipe) => recipe.item.toLowerCase() === recipes.toLowerCase()
 			);
-			const rec = Object.assign({}, searchedRecipes.recipe)
-			rec.foreach(r =>{
-				console.log(r);
-			})
+			
+			
 			if (searchedRecipes) {
 				textMcRecipes.textContent = searchedRecipes.item;
 			} else {
@@ -90,6 +89,114 @@ const getRecipes = () => {
 			console.error("Błąd podczas pobierania danych:", error);
 		});
 };
-btnSearchRecipes.addEventListener("click", getRecipes);
+const rec = () =>{
+	const recipes2 = inputSearchRecipes.value;
+	const URL_RECIPES2 = API_URL + API_RECIPES;
+	fetch(URL_RECIPES2, options).then((res)=>res.json()).then((data)=>{
+	
+		const searchedRecipes = data.find(
+			(recipe) => recipe.item.toLowerCase() === recipes2.toLowerCase()
+		);
+		//console.log(crafting[1]);
+		if(searchedRecipes){
+			const re = searchedRecipes.recipe
+			
+			for(let i=0;i<=9;i++){
+				//console.log(re[i]);
+				if(i==0){
+					// crafting[0].textContent = re[0].textContent
+					if(re[0] !== null){
+						crafting[0].textContent = re[0]
+
+						
+					}else{
+						console.log("Błąd, wartość pola to null!");
+					}
+				}else if(i==1){
+					if(re[1] !== null){
+						crafting[1].textContent = re[1]
+
+						
+					}else{
+						console.log("Błąd, wartość pola to null!");
+					}
+				}
+				else if(i==2){
+					if(re[2] !== null){
+						crafting[2].textContent = re[2]
+
+						
+					}else{
+						console.log("Błąd, wartość pola to null!");
+					}
+				}
+				else if(i==3){
+					if(re[3] !== null){
+						crafting[3].textContent = re[3]
+
+						
+					}else{
+						console.log("Błąd, wartość pola to null!");
+					}
+				}
+				else if(i==4){
+					if(re[4] !== null){
+						crafting[4].textContent = re[4]
+
+						
+					}else{
+						console.log("Błąd, wartość pola to null!");
+					}
+				}
+				else if(i==5){
+					if(re[5] !== null){
+						crafting[5].textContent = re[5]
+
+						
+					}else{
+						console.log("Błąd, wartość pola to null!");
+					}
+				}
+				else if(i==6){
+					if(re[6] !== null){
+						crafting[6].textContent = re[6]
+
+						
+					}else{
+						console.log("Błąd, wartość pola to null!");
+					}
+				}
+				else if(i==7){
+					if(re[7] !== null){
+						crafting[7].textContent = re[7]
+
+						
+					}else{
+						console.log("Błąd, wartość pola to null!");
+					}
+				}
+				else if(i==8){
+					if(re[8] !== null){
+						crafting[8].textContent = re[8]
+
+						
+					}else{
+						console.log("Błąd, wartość pola to null!");
+					}
+				}
+			}
+		}
+		// data.forEach(element => {
+		// 	const elem = element.recipe
+			
+		// 	elem.forEach(el =>{
+		// 		console.log(el);
+		// 	})
+		// });
+	})
+}
+
+btnSearchRecipes.addEventListener('click', rec)
+// btnSearchRecipes.addEventListener("click", getRecipes);
 btnSearchBlock.addEventListener("click", getBlocks);
 btnSearchItems.addEventListener("click", getItems);
